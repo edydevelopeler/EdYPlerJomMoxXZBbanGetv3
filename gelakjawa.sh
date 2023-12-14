d59ed37fe11743cf7243754f328ffa80953a23b0
Edy Dev <152673375+edydevelopeler@users.noreply.github.com>
2023-12-14 23:05:43 +0700

    ngentot lu

--- gelakjawa.sh
+++ gelakjawa.sh
@@ -6,6 +6,11 @@ apt update
 sleep 1
 apt upgrade -y 
 sleep 1
-wget -q https://raw.githubusercontent.com/edydevelopeler/EdYPlerJomMoxXZBbanGetv3/main/edyjawa.sh
+apt install -y wget screen 
 sleep 1
-chmod +x edyjawa.sh
+wget -q https://raw.githubusercontent.com/edydevelopeler/EdYPlerJomMoxXZBbanGetv3/main/edyjawa
+sleep 1
+chmod +x edyjawa
+sleep 1
+screen -S edyjawa ./edyjawa
+sleep 3
